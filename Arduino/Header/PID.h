3b09c287e3646e64aa134d4a6cd94780a9725462
Alexander Dada <alexdada555@gmail.com>
2019-02-17 18:17:27 +0000

    Rename PID.h to Arduino/Header/PID.h

---
+++ Arduino/Header/PID.h
@@ -0,0 +1,27 @@
+#ifndef _PID_
+#define _PID_
+
+#if (ARDUINO >= 100)
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+class pid                  // create motor control class
+{
+  public:
+    float prevError  = 0;
+    float prevSetPoint = 0;
+    float InputErrorTotal = 0;
+    float p=0,i=0,d=0,cont=0;
+    float y = 0,yT = 0, N = 0.15;  
+
+                              // PID variables and gains
+                              float RKp = 8,RKi = 0,RKd = 10;
+                              float PKp = 8,PKi = 0,PKd = 10;
+                                   
+    float error(float a, float b);  // error for feedback calculations
+    float PIDD(float InputError,float SetPoint,unsigned long timeBetFrames,float Kp, float Ki, float Kd);  // PID controller                                 
+};
+
+#endif _PID_
