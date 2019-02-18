d729a587cd8cfeacb4479e2581683ea3e32a496a
Alexander Dada <alexdada555@gmail.com>
2019-02-18 01:11:00 +0000

    Add files via upload

--- Arduino/MobilePlatforms.ino
+++ Arduino/MobilePlatforms.ino
@@ -1,14 +1,16 @@
-
 #include "PID.h"
 #include "Motor.h"
-
+#include "RosInOut.h"
 
 unsigned long timer = 0;
 unsigned long timeBetFrames = 0;
-
 float timeStep = 0.1;
 
 Motor motor;
+pid Pid;
+RosInOut ros;
+
+int xval,yval;
 
 void setup()
 {
@@ -18,6 +20,10 @@ void setup()
 void loop()
 {
   timer = millis();
+  
+  xval = ros.readSerial();
+  yval = ros.readSerial()+1;
+  
   motor.Forward(200);
   timeBetFrames = millis() - timer;
   delay((timeStep*100) - timeBetFrames); //Run at 100Hz
