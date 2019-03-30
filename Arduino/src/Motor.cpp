5cfa80268fe8733bfb388025c617f6f730d35a1d
Alexander Dada <alexdada555@gmail.com>
2019-03-30 03:44:04 +0000

    Add files via upload

--- Arduino/src/Motor.cpp
+++ Arduino/src/Motor.cpp
@@ -71,10 +71,25 @@ void Motor::Forward(int Target,long timeBetFrames,int enc1,int enc2,int enc3,int
 
 void Motor::Backward(int Target,long timeBetFrames,int enc1,int enc2,int enc3,int enc4)
 {
-  RunMotors(M1,0,E1,Target);
-  RunMotors(M2,1,E2,Target);
-  RunMotors(M3,0,E3,Target);
-  RunMotors(M4,1,E4,Target);
+  Pid[0].Setpoint = Target;
+  Pid[1].Setpoint = Target;
+  Pid[2].Setpoint = Target;
+  Pid[3].Setpoint = Target;
+  
+  Pid[0].Input = Pid[0].velocity(enc1,timeBetFrames);
+  Pid[1].Input = Pid[1].velocity(enc2,timeBetFrames);
+  Pid[2].Input = Pid[2].velocity(enc3,timeBetFrames);
+  Pid[3].Input = Pid[3].velocity(enc4,timeBetFrames);
+  
+  PID1.Compute();
+  PID2.Compute();
+  PID3.Compute();
+  PID4.Compute();
+  
+  RunMotors(M1,0,E1,Pid[0].Output);
+  RunMotors(M2,1,E2,Pid[1].Output);
+  RunMotors(M3,0,E3,Pid[2].Output);
+  RunMotors(M4,1,E4,Pid[3].Output);
 }
 
 void Motor::Left(int Target,long timeBetFrames,int enc1,int enc2,int enc3,int enc4)
