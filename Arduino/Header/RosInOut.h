f410c94b23152fce965a6c1fcf5493fcedad63b3
Alexander Dada <alexdada555@gmail.com>
2019-04-03 07:09:05 +0100

    Add files via upload

--- Arduino/Header/RosInOut.h
+++ Arduino/Header/RosInOut.h
@@ -11,20 +11,17 @@ class RosInOut                  // create ROS Communication class
 {
   private:
     char serialInput;
-    int char2int[50];
+    int char2int[50] = {0};
     int increment = 0;
     int VectorX = 0;
     int VectorY = 0;
 
   public:
     char ID;
-    char readPosID();       
-    int readPosX(); 
-    int readPosY(); //read in serial data and convert it to an integer
-    
-    char readTarID(); 
-    int readTarX(); 
-    int readTarY();
+    char readID();       
+    int *readPos(); //read in serial data and convert it to an integer
+    int *readTar();
+    int *readAng();
 };
 
 #endif  __ROSINOUT__
