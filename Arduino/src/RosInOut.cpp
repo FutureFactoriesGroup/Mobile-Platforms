bde7d664241c39c49ac4400563d36a8618b32dd6
Alexander Dada <alexdada555@gmail.com>
2019-04-12 22:42:29 +0100

    Add files via upload

--- Arduino/src/RosInOut.cpp
+++ Arduino/src/RosInOut.cpp
@@ -19,81 +19,48 @@ char RosInOut::readID()
   return(ID);
 }
 
-int *RosInOut::readPos()
+void RosInOut::readPos()
 {
-  static int VectorXY[2];
-  if(Serial.available())
+  char id = readID();
+  if(id == 'A')
   {
-     serialInput = Serial.read();
-     char2int[increment] = (int)serialInput - 48;
-     increment++;
-        
-     if (serialInput == '>')
-     {
-        increment = 0;
-        
-        VectorXY[0] = char2int[3]*100;
-        VectorXY[0] += char2int[4]*10;
-        VectorXY[0] += char2int[5]*1;
-
-        VectorXY[1] = char2int[7]*100;
-        VectorXY[1] += char2int[8]*10;
-        VectorXY[1] += char2int[9]*1;
-        *char2int = 0;
-     }
-  }
-  return(VectorXY);
-}
-
-int *RosInOut::readAng()
-{
-  static int Angle[1];
-  if(Serial.available())
-  {
-     serialInput = Serial.read();
-     char2int[increment] = (int)serialInput - 48;
-     increment++;
-        
-     if (serialInput == '>')
-     {
-        increment = 0;
-        
-         Angle[0] = char2int[11]*100;
-         Angle[0] += char2int[12]*10;
-         Angle[0] += char2int[13]*1;
-        *char2int = 0;
-     }
-  }
-  return(Angle);
+     while(Serial.available() > 0)
+     { 
+        serialInput = Serial.read();
+        char2int[numPoints] =  Serial.parseInt();
+        Position[numPoints] = char2int[numPoints];
+        numPoints++;
+     
+        if (serialInput == '\n') 
+        {
+           *char2int = 0;
+           *Position = {0};
+           numPoints = 0;
+           break;
+        }
+      }
+   }
 }
 
 void RosInOut::readTar()
 {
-  if(Serial.available())
+  char id = readID();
+  if(id == 'P')
   {
-     serialInput = Serial.read();
-     char2int[increment] = (int)serialInput - 48;
-     increment++;
-
-     if (serialInput == '>')
-     {
-        increment = 0;
-        int dim = char2int[3]*100;
-        dim += char2int[4]*10;
-        dim += char2int[5]*1;
-        
-        for(int x = 0; x< ((dim+1)/4); x++)
+     while(Serial.available() > 0)
+     { 
+        serialInput = Serial.read();
+        char2int[numPoints] =  Serial.parseInt();
+        Path[numPoints] = char2int[numPoints];
+        numPoints++;
+     
+        if (serialInput == '\n') 
         {
-          Path[x] = char2int[7+4*x]*100;
-          Path[x] += char2int[8+4*x]*10;
-          Path[x] += char2int[9+4*x]*1;
-
-          Path[x+1] = char2int[11+4*x]*100;
-          Path[x+1] += char2int[12+4*x]*10;
-          Path[x+1] += char2int[13+4*x]*1;
-          numPoints++;
+          *char2int = 0;
+          *Path = {0};
+          numPoints = 0;
+          break;
         }
-        *char2int = 0;
-     }
+      }
   }
 }
