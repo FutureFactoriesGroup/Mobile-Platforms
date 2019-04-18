3e0a082f0e87da2b9860fb496444a322128d7cfe
Yadav Dosieah <44868771+YadavDosieah@users.noreply.github.com>
2019-04-18 13:14:48 +0100

    Added basic target speed code

--- ROS/PlatformNode.py
+++ ROS/PlatformNode.py
@@ -7,11 +7,15 @@ import serial
 from std_msgs.msg import String
 import math
 import numpy as np
+
 x = []
 y = []
 serialList = []
 PtIndex = 0
 pub = None
+pathlength = 0
+Speed = 0
+PreviousTime = 0
 #print("this is a test")
 rospy.init_node('TransportNode', anonymous=True)
 pub = rospy.Publisher('/PlatformError', String, queue_size=10)
@@ -57,10 +61,15 @@ def callback(data):
 	global x
 	global y
 	global PtIndex
+	global Speed
+	global PreviousTime
 	message = str(data.data)
 	#pub.publish(data.data)
 	if message.startswith("31"):
 		if message[4:7] == "022" and PathSent == True:
+			StartTime = time.time()*1000
+			TimeStep = StartTime - PreviousTime
+			PreviousTime = StartTime
 			#pub.publish("022")
 			info = message.split("(")
 			message = info[1]
@@ -96,16 +105,45 @@ def callback(data):
 				alphaError = alphaError + (math.pi)*200.0
 			pub.publish("DesiredAlpha: " + str(desiredAlpha))
 
-			#desiredAlpha = (desiredAlpha + (math.pi)*100.0) % ((math.pi)*200.0)
+			DistanceErr = ((Xerr**2)+(Yerr**2))**0.5
 
-			#pub.publish("final DesiredAlpha: " + str(desiredAlpha))
-			# if(desiredAlpha<0):
-			# 	desiredAlpha = desiredAlpha + 2*math.pi
-			# pub.publish("alpha: " + str(alpha))
-			# if alphaError > (math.pi*100):
-			# 	alphaError = (200*math.pi) -  alphaError
-			# elif alphaError < -math.pi:
-			# 	alphaError = 2*math.pi + alphaError
+			if(alphaError > 5 or alphaError < -5): #In centi-rad
+				Rotate = True
+				if (alphaError > 10):
+					#rotate anticlockwise
+					try:
+						arduino.write("-25,1") #(Speed CCW,Rotate)
+						pub.publish("-25,1")
+					except serial.SerialException:
+						pass
+				elif (alphaError < -10):
+					try:
+						arduino.write("25,1")# (Speed CW,Rotate)
+						pub.publish("25,1")
+					except serial.SerialException:
+						pass
+			else:
+				#Stop rotating
+				Rotate = False
+
+			if(Rotate == False and (DistanceErr > 5)):	#in mm
+				try:
+					arduino.write("25,2") #Translate
+					pub.publish("25,2")
+					Speed = 0
+				except serial.SerialException:
+					pass
+
+			elif(Rotate == False and (DistanceErr < 5)):
+				#PtIndex++
+				try:
+					arduino.write("0,0") #All stop
+					pub.publish("0,0")
+					Speed = 0
+				except serial.SerialException:
+					pass
+				if(PtIndex > pathlength-1)
+					PathSent = False
 
 			ErrorVector = "(" + str(int(Xerr))+","+str(int(Yerr))+","+str(int(alphaError)) + ")"
 			pub.publish(ErrorVector)
@@ -121,14 +159,15 @@ def callback(data):
 
 
 		elif message[4:7] == "019":
+			global pathlength
 			pub.publish("019")
 			info = message.split("(")
 			message = info[1]
 			message = message[:-65].split(",")
-			length = int(message[0])
+			pathlength = int(message[0])
 			x = []
 			y = []
-			for i in range(length):
+			for i in range(pathlength):
 				x.append(int(message[(2*i)+1]))
 				y.append(int(message[(2*i)+2]))
 			PathSent = True
@@ -158,94 +197,3 @@ try:
 
 except rospy.ROSInterruptException:
     pass
-
-#!/usr/bin/env python
-#import rospy
-#import sys
-#import time
-#import serial
-#from std_msgs.msg import String
-
-#arduino = serial.Serial('/dev/ttyACM0', 9600,timeout = 1)
-#arduino = serial.Serial('/dev/ttyUSB0', 9600,timeout = 1)
-#time.sleep(2)
-
-# L = 3.4
-# W = 1.6
-#i = 0
-#PathSent = False
-#path2 = ['<'+'P','047','100','100','200','200','300','300','400','400','500','500'+'>'+"\n"]
-#pastring2 = ",".join(path)
-
-#rospy.init_node('TransportNode', anonymous=True)
-#pub = rospy.Publisher('/PlatformError', String, queue_size=10)
-#r = rospy.Rate(10)
-
-#def convert(input,old_min,old_max,new_min,new_max):
-        # take original value rangeand map it to a new range
-#        old_value =  input
-#        new_value = ( ( old_value - old_min ) / (old_max - old_min) ) * (new_max - new_min) + new_min
-#        return(new_value)
-
-#def serialprint(data):
-#        global PathSent
-
-#        message = str(data.data)
-
-#        if message[4:7] == "022" and PathSent == True:
-#                info = message.split("(")
-#                message = info[1]
-#                message = (message[:-65]).split(",")
-#                NID = message[0]
-#                X  = message[1]
-#                Y  = message[2]
-#                a  = message[3]
-
-#                pos = ['<'+ NID,X,Y,a +'>']
-#                postring = ",".join(pos)
-#                print(postring)
-#                arduino.write(postring)
-#                rospy.loginfo(postring)
-	        #while not rospy.is_shutdown():
-               	#	pub.publish(postring)
-                #	r.sleep()
-
-
-#        elif message[4:7] == "019":
-#                PathSent = True
-#                NID = 'P'
-#                Length = message[11:14]
-#                info = message.split("(")
-#                message = info[1]
-#                message = message[:-65].split(",")
-#                x = message[0]
-#                xy = ' '
-
-#                for i in range(1,x):
-#                        xy += message[i]
-#
-#                path = ['<'+ NID,Length,xy+'>']
-#                pathstring = ",".join(path)
-#                print(pathring)
-#                arduino.write(pathstring)
-#                rospy.loginfo(pathring)
-	       # while not rospy.is_shutdown():
-               # 	pub.publish(pathstring)
-               # 	r.sleep()
-
-
-#def listener():
-	# In ROS, nodes are uniquely named. If two nodes with the same
-	# name are launched, the previous one is kicked off. The
-	# anonymous=True flag means that rospy will choose a unique
-	# name for our 'listener' node so that multiple listeners can
-	# run simultaneously.
-#        rospy.init_node('TransportNode', anonymous=True)
-#        rospy.Subscriber("/Transport", String, serialprint)
-#	rospy.spin() #simply keeps python from exiting until this node is stopped
-
-#if __name__ == '__main__':
-#	try:
-#		listener()
-#        except rospy.ROSInterruptException:
-#                pass
