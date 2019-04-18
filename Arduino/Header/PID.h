6456ad16c0a67fd8c2996ee18831ac393e74a5bf
Alexander Dada <alexdada555@gmail.com>
2019-04-18 20:06:52 +0100

    Add files via upload

--- Arduino/Header/PID.h
+++ Arduino/Header/PID.h
@@ -14,7 +14,7 @@ class pid                  // create PID class
     int prevPos = 0;
     float c;
     double Input,Output,Setpoint;
-    double Kp = 0.85,Ki = 0.012,Kd = 0; // PID variables and gains
+    double Kp = 8.5,Ki = 0.012,Kd = 0; // PID variables and gains
     
     int velocity(int pos, long tim);  // motor angular velocity                               
     float error(float a, float b);    // error for feedback calculations
